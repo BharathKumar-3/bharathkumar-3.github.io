954dbcadac0d977fd61573c299aa28383d4be564
BharathKumar-3 <bharathkumar13619@gmail.com>
2023-03-08 18:52:51 +0530

    emailadded

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 let submit_button = document.getElementById("submit-button");
-const fs = require("fs");
 submit_button.addEventListener("click",(event)=>{
     let name = document.getElementById("name");
     let email = document.getElementById("email");
     let message = document.getElementById("message");
-    add_value({
-        name : name.value,
-        email : email.value,
-        message:message.value
-    });
+    console.log(add_value({
+        name : name.value+" "+email.value,
+        message : message.value
+    }));
+    console.log(name.value," ",email.value," ",message.value)
 })
-const add_value = (data)=>{
-    fs.appendFile("message.txt","hello",(err)=>{
-        if(err){
-            console.log(err);
-        }
+async function add_value(data){
+    const response = await fetch("http://localhost:8080/",{
+        method :"POST",
+        mode : "cors",
+        headers:{"Content-Type": "application/json"},
+        body:JSON.stringify(data)
     })
-    console.log(data);
-    console.log(data.name," ",data.email," ",data.message)
+    return response.json();
 }
